af2e0789a09886a50f15ea9f3a7f49607d569e4d
ajharrisii <ajharris2019@gmail.com>
2025-02-11 22:10:11 -0600

    Work Constant

---
+++ FrontEnd/assets/js/index.js
@@ -0,0 +1,15 @@
+
+
+
+const work = {
+    "id": 1,
+    "title": "Tahina lampshade",
+    "imageUrl": "http://localhost:5678/images/abajour-tahina1651286843956.png",
+    "categoryId": 1,
+    "userId": 1,
+    "category": {
+      "id": 1,
+      "name": "Objects"
+    }
+  }
+  // create a function that uses that uses the work variable
